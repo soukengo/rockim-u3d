b6f90096e056e2d9c0e72e5744751389642fa6d8
soukengo <soukengo@163.com>
2023-06-07 18:16:39 +0800

    first commit

---
+++ Assets/RockIM/Client/Scripts/Chat/ChatMenuItem.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace RockIM.Client.Scripts.Chat
+{
+    [Serializable]
+    public class ChatMenuItem
+    {
+        public string key;
+
+        public Sprite sprite;
+    }
+}
